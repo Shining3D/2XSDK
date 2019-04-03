c4bbc37065e065f7fbcb2cfadb88be3c8db183f5
chenjinming <chenjinming@shining3d.com>
2019-04-03 10:53:40 +0800

    2nd part

---
+++ language_ts.bat
@@ -0,0 +1,46 @@
+::使用前需要根据本机的文件路径修改下面的目录： qt工具lupdate的目录，本地源码目录 输出ts文件路径（需要指向已经存在的ts文件）
+
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Chinese_China -ts ../doc/EinScan-ts/Language_zh_CN.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Japanes_Japan -ts ../doc/EinScan-ts/Language_ja.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_ru.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_pl.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_he.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_fr.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_it.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_de.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_ko.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_zh_TW.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_Turkey.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/qt_platform/qt_platform.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/Language_es.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_Turkey.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Chinese_China -ts ../doc/EinScan-ts/tool_zh_CN.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Japanes_Japan -ts ../doc/EinScan-ts/tool_ja.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_ru.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_pl.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_he.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_fr.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_it.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_de.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_ko.ts  
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_zh_TW.ts 
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/einscan_tool/einscan_tool.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/tool_es.ts 
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Chinese_China -ts ../doc/EinScan-ts/userfeedback_zh_CN.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Chinese_China -ts ../doc/EinScan-ts/userfeedback_zh_TW.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Chinese_China -ts ../doc/EinScan-ts/userfeedback_Turkey.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Japanes_Japan -ts ../doc/EinScan-ts/userfeedback_ja.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/userfeedback_ru.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/userfeedback_pl.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/userfeedback_he.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/userfeedback_fr.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/userfeedback_it.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/userfeedback_de.ts
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/userfeedback_ko.ts  
+D:\Qt\Qt5.6.2\5.6\msvc2013_64\bin\lupdate -pro ../src/userfeedback/userfeedback.pro -target-language Russian_Russia -ts ../doc/EinScan-ts/userfeedback_es.ts 
+
+
+
+
+
+
+
+
